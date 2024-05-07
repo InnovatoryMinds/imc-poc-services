5a3c42bb771b2fb048e73054437c711850572d5b
vamsiborusu28 <vamsiborusu2000@gmail.com>
2024-05-07 16:04:14 +0530

    logger and exceptions are added to rabbitmq publisher

--- LeadGenerationRabbitMQ/LeadGenerationPublisher/src/main/java/com/mmc/app/platform/Controllers/LeadGenerationController.java
+++ LeadGenerationRabbitMQ/LeadGenerationPublisher/src/main/java/com/mmc/app/platform/Controllers/LeadGenerationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class LeadGenerationController {
@@ -16,14 +18,18 @@ public class LeadGenerationController {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    public static final Logger logger = LoggerFactory.getLogger(LeadGenerationController.class);
 
     @PostMapping("/leadgeneration")
-    public String LeadGenerationPublisher(@RequestBody List<LeadGeneration> leadGenerations){
-//        System.out.println(leadGenerations);
+    public String LeadGenerationPublisher(@RequestBody List<LeadGeneration> leadGenerations) {
+        // System.out.println(leadGenerations);
+        logger.info("LeadGeneration Data is" + leadGenerations);
         try {
-            rabbitTemplate.convertAndSend(LeadGenerationConfig.LeadGenerationExchange, LeadGenerationConfig.LeadGenerationRouteKey, leadGenerations);
-        } catch (Exception exception){
-            return  "connection not established please connect to rabbit mq server";
+            rabbitTemplate.convertAndSend(LeadGenerationConfig.LeadGenerationExchange,
+                    LeadGenerationConfig.LeadGenerationRouteKey, leadGenerations);
+        } catch (Exception exception) {
+            logger.error("connection not established please connect to rabbit mq server");
+            return "connection not established please connect to rabbit mq server";
         }
 
         return "Data sent to the rabbitmq";
